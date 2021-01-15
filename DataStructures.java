fbb4912a1339707f2de291438f520c6f3be744bc
marilynm1119 <marquez_mari@yahoo.com>
2021-01-15 01:43:20 +0000

    Testing repl.it

--- DataStructures.java
+++ DataStructures.java
@@ -1,7 +1,7 @@
 class DataStructures {
   public static void main(String[] args) {
     System.out.println("Hello world!");
-
+    //commit
     long startTime = System.currentTimeMillis();
     String[] array = { "a", "b", "c", "d" };
     for (String s : array) {
